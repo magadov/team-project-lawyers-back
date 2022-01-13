db633a7b8815035d42a68e5d038fd3e88028a171
usman <dzakaev.94@mail.ru>
2022-01-13 14:09:10 +0300

    начальная

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const chalk = require("chalk")
+const port = 3002;
+const url =
+  "mongodb+srv://usman:Usman1994@cluster0.rlomm.mongodb.net/lawyers?retryWrites=true&w=majority";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use(require("./routes"));
+
+const connectAndStartServer = async () => {
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    app.listen(port, () => {
+      console.log(chalk.green(`Успешно соединились. Порт ${port}`));
+    });
+  } catch (e) {
+    console.log(chalk.bgRed.white(`Ошибка при подключении: ${e.toString()}`));
+  }
+};
+
+connectAndStartServer();
