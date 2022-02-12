5fd923a6c3375043ffa9818a233144c2a8fb7ede
usman <dzakaev.94@mail.ru>
2022-02-12 13:33:02 +0300

    usman

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,7 @@ const { Router } = require("express");
 
 const router = Router();
 
+router.use("/categories", require("./categories.route"));
+router.use("/users", require("./users.route"));
+
 module.exports = router;
